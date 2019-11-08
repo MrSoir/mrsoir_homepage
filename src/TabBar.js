b34938c5ebe4bfdf181740443d69031e92a20018
Hippo <support@mrsoir.com>
2019-11-08 18:16:04 +0100

    bashed commit

--- src/TabBar.js
+++ src/TabBar.js
@@ -1,88 +1,238 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect, useRef } from 'react';
 import { withRouter } from "react-router-dom";
 import './TabBar.css';
 
-class TabBar extends Component{
-/*	constructor(props){
-		super(props);
-	}*/
-	tabClicked(tabid, tabCallback){
-		tabCallback(tabid);
+
+function MenuButton({showButton=false,
+	 									 onMenuEntered=()=>{},
+									   onMenuClicked=()=>{}}){
+	function genShowBarsStyle(id){
+		let style = {
+			top: '25%',
+			left: '50%',
+			width: '80%',
+			opacity: '1',
+			transform: 'translate(-50%, -50%)',
+			backgroundColor: 'var(--main-bg-color)'
+		};
+		switch(id){
+			case 1:
+				style['top'] = '50%';
+				break;
+			case 2:
+				style['top'] = '75%';
+				break;
+		}
+		return style;
 	}
-	render(){
-    return (
-      <div className="TabBar HeadingTextSize">
-   		{this.props.tabs.map((tab, i) =>{
-   			let isLast = (i >= (this.props.tabs.length-1));
-				return <TabElement key={i}
-									isLast={isLast}
-									tab={tab}
-									tabid={i}
-									tabClicked={()=>this.tabClicked(i, this.props.tabCallback)}
-									selected={(i % 2 ? true : false)}
-				/>
-			}
-      	)}
+	function genHideBarsStyle(id){
+		let style = {
+			top: '50%',
+			left: '50%',
+			width: '50%',
+			backgroundColor: 'red'
+		};
+		switch(id){
+			case 0:
+				style['top'] = '50%';
+				style['transform'] = 'translate(-50%, -50%) rotate(-45deg)';
+				break;
+			case 1:
+				style['top'] = '50%';
+				style['transform'] = 'translate(-50%, -50%) rotate(45deg)';
+				break;
+			case 2:
+				style['opacity'] = '0';
+				break;
+		}
+
+		return style;
+	}
+
+	let btnStyls = [];
+	for(let i=0; i < 3; ++i){
+		btnStyls.push( showButton ? genShowBarsStyle(i) : genHideBarsStyle(i) );
+	}
+	return (
+		<div className="MenuButtonTB"
+				 onMouseOver={onMenuEntered}
+				 onClick={onMenuClicked}>
+			<div className="MenuButtonItemTB" style={btnStyls[0]}>
+			</div>
+			<div className="MenuButtonItemTB" style={btnStyls[1]}>
+			</div>
+			<div className="MenuButtonItemTB" style={btnStyls[2]}>
+			</div>
+		</div>
+	)
+}
+function MinimizedTabBar({tabs, tabClicked}){
+	const mainRef = useRef();
+	const menuItemsDiv = useRef();
+	const [showMenuItems, setShowMenuItems] = useState(false);
+
+	function onMenuEntered(){
+		if( window.mobilecheck() )return;
+		setShowMenuItems(true);
+	}
+	function onMenuClicked(){
+		if( !window.mobilecheck() )return;
+		setShowMenuItems(!showMenuItems);
+	}
+	function hideMenuItemsDiv(){
+		const mid = menuItemsDiv.current;
+		if(!mid)return;
+		mid.style.width = '0px';
+		mid.style.border = '0px solid var(--main-bg-color)';
+	}
+	function showMenuItemsDiv(){
+		const mid = menuItemsDiv.current;
+		if(!mid)return;
+		mid.style.width = '200px';
+		mid.style.border = '2px solid var(--main-bg-color)';
+	}
+	function onMouseMenuItemsLeft(){
+		setShowMenuItems(false);
+	}
+
+	useEffect(()=>{
+		if(showMenuItems){
+			showMenuItemsDiv();
+		}else{
+			hideMenuItemsDiv();
+		}
+	}, [showMenuItems]);
+	useEffect(()=>{
+	}, []);
+
+	return (
+		<div className="MinimizedTabBarTB"
+				 ref={mainRef}>
+			<div className="MenuButtonDivTB">
+				<MenuButton onMenuEntered={onMenuEntered}
+										onMenuClicked={onMenuClicked}
+										showButton={!showMenuItems}/>
+			</div>
+			<div className="MenuItemsDivTB  TabBar HeadingTextSize"
+					 ref={menuItemsDiv}
+					 onMouseLeave={onMouseMenuItemsLeft}>
+				<div className="ListMTB">
+					{tabs.map((tab, i)=>{
+						return <TabElement key={i}
+											isLast={true}
+											tab={tab}
+											tabid={i}
+											tabClicked={tabClicked}
+											selected={(i % 2 ? true : false)}
+						/>
+					})}
+				</div>
+			</div>
+		</div>
+	)
+}
+function TabBar({tabCallback, tabs}){
+	const mainRef = useRef();
+	const [minimize, setMinimize] = useState( shouldMinimize() );
+
+	function shouldMinimize(){
+		const [vw, vh] = [window.innerWidth, window.innerHeight];
+		const minDim = 800;
+		return vh < minDim || vw < minDim;
+	}
+	function onResize(){
+		setMinimize( shouldMinimize() );
+	}
+
+	useEffect(()=>{
+		window.addEventListener('resize', onResize);
+		return ()=>{
+			window.removeEventListener('resize', onResize);
+		}
+	})
+
+	function tabClicked(tabid){
+		tabCallback && tabCallback(tabid);
+	}
+
+	function createBigTabBar(){
+		return (
+			<div className="TabBar HeadingTextSize"
+					 ref={mainRef}>
+				{tabs.map((tab, i) =>{
+					let isLast = (i >= (tabs.length-1));
+					return <TabElement key={i}
+										isLast={isLast}
+										tab={tab}
+										tabid={i}
+										tabClicked={()=>tabClicked(i)}
+										selected={(i % 2 ? true : false)}
+					/>
+				}
+				)}
 				<div className="TabSeparator">
 				</div>
-      </div>
-    );
-  }
+			</div>
+		);
+	}
+	function createMinimizedTabBar(){
+		return (
+				<MinimizedTabBar tabClicked={tabClicked}
+												 tabs={tabs}
+				/>
+		);
+	}
+	return minimize ? createMinimizedTabBar() : createBigTabBar();
 }
 
-class TabElement extends Component{
-	constructor(props){
-		super(props);
+function TabElement({tab, isLast, tabid, tabClicked=()=>{}}){
+	const indicator = React.createRef();
 
-		this.indicator = React.createRef();
-		this.tabMouseEnter = this.tabMouseEnter.bind(this);
-		this.tabMouseOut = this.tabMouseOut.bind(this);
-	}
-	tabSelected(){
-		const ind = this.indicator.current;
-		if ( !this.props.tab.selected ) {
+	function tabSelected(){
+		const ind = indicator.current;
+		if ( !tab.selected ) {
 			ind.classList.add('Selected');
 		}else{
 			ind.classList.remove('Selected');
 		}
 	}
-	tabMouseEnter(){
-		if ( !this.props.tab.selected ) {
-			const ind = this.indicator.current;
+	function tabMouseEnter(){
+		if ( !tab.selected ) {
+			const ind = indicator.current;
 			ind.classList.add('Hovered');
 		}
 	}
-	tabMouseOut(){
-		if ( !this.props.tab.selected ) {
-			const ind = this.indicator.current;
+	function tabMouseOut(){
+		if ( !tab.selected ) {
+			const ind = indicator.current;
 			ind.classList.remove('Hovered');
 		}
 	}
-	render(){
-		let isLast = !!this.props.isLast;
-		let tabLinkClass = "TabLink" + (isLast ? " LastTabElement" : "");
-		let tabElementClass = "TabElement HeadingTextSize";
-		if(this.props.tab.selected){
-			tabLinkClass += ' Selected';
-			tabElementClass += ' Selected';
-		}
 
-		return(
-		   <div className={tabElementClass}
-			 			onClick={()=>this.props.tabClicked(this.props.tabid)}
-						onMouseEnter={()=>this.tabMouseEnter()}
-						onMouseOut={()=>this.tabMouseOut()} >
-      		<div className={tabLinkClass}
-      			  key={this.props.tabid}
-							onMouseEnter={()=>this.tabMouseEnter()}
-							onMouseOut={()=>this.tabMouseOut()}>
-      			{this.props.tab.name}
-      		</div>
-      		<div className={"TabIndicator" + (this.props.tab.selected ? " Selected" : "")}
-      			  ref={this.indicator}></div>
-   		</div>
-		);
+	let tabLinkClass = "TabLink" + (isLast ? " LastTabElement" : "");
+	let tabElementClass = "TabElement HeadingTextSize";
+	if(tab.selected){
+		tabLinkClass += ' Selected';
+		tabElementClass += ' Selected';
 	}
+
+
+	return(
+	   <div className={tabElementClass}
+		 			onClick={()=>tabClicked(tabid)}
+					onMouseEnter={()=>tabMouseEnter()}
+					onMouseOut={()=>tabMouseOut()} >
+    		<div className={tabLinkClass}
+    			  key={tabid}
+						onMouseEnter={()=>tabMouseEnter()}
+						onMouseOut={()=>tabMouseOut()}>
+    			{tab.name}
+    		</div>
+    		<div className={"TabIndicator" + (tab.selected ? " Selected" : "")}
+    			  ref={indicator}>
+				</div>
+ 		</div>
+	);
 }
 
 export default withRouter(TabBar);
